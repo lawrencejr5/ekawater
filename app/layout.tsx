eb32e6cf13948080316889ecde4a8f51f828b86a
lawrencejr5 <oputalawrence@gmail.com>
2026-08-02 15:31:43 +0100

    added fade in on scroll animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./page.css";
 
 export const metadata: Metadata = {
   title: "EKA Water Enhancer — One Squeeze. Endless Flavor.",
